726f4aa5eca3a5a7becede5fa2cdfa7aa78f9a2c
Forwardslash-development <forwardslash.development@gmail.com>
2021-05-17 14:13:14 -0500

    updated schema and resolvers

--- server/src/schema.js
+++ server/src/schema.js
@@ -1,53 +1,75 @@
 const { gql } = require('apollo-server');
 
 const typeDefs = gql`
+  type Query {
+    launches( 
+      """
+      The number of results to show. Must be >= 1. Default = 20
+      """
+      pageSize: Int
+      """
+      If you add a cursor here, it will only return results _after_ this cursor
+      """
+      after: String
+    ): LaunchConnection!
+    launch(id: ID!): Launch
+    me: User
+  }
+
+  type Mutation {
+    bookTrips(launchIds: [ID]!): TripUpdateResponse!
+    cancelTrip(launchId: ID!): TripUpdateResponse!
+    login(email: String): User
+    }
+
+  type TripUpdateResponse {
+    success: Boolean!
+    message: String
+    launches: [Launch]
+    }
+
+"""
+Simple wrapper around our list of launches that contains a cursor to the
+last item in the list. Pass this cursor to the launches query to fetch results
+after these.
+"""
+
+  type LaunchConnection {
+    cursor: String!
+    hasMore: Boolean!
+    launches: [Launch]!
+    }
+
   type Launch {
   id: ID!
   site: String
   mission: Mission
   rocket: Rocket
   isBooked: Boolean!
-}
-type Rocket {
-  id: ID!
-  name: String
-  type: String
-}
+  }
 
-type User {
-  id: ID!
-  email: String!
-  trips: [Launch]!
-  token: String
-}
+  type Rocket {
+    id: ID!
+    name: String
+    type: String
+  }
 
-type Mission {
-  name: String
-  missionPatch(size: PatchSize): String
-}
+  type User {
+    id: ID!
+    email: String!
+    trips: [Launch]!
+    token: String
+  }
 
-enum PatchSize {
-  SMALL
-  LARGE
-}
+  type Mission {
+    name: String
+    missionPatch(size: PatchSize): String
+  }
 
-type Query {
-  launches: [Launch]!
-  launch(id: ID!): Launch
-  me: User
-}
-
-type Mutation {
-  bookTrips(launchIds: [ID]!): TripUpdateResponse!
-  cancelTrip(launchId: ID!): TripUpdateResponse!
-  login(email: String): User
-}
-
-type TripUpdateResponse {
-  success: Boolean!
-  message: String
-  launches: [Launch]
-}
+  enum PatchSize {
+    SMALL
+    LARGE
+  }
 `;
 
 module.exports = typeDefs;
